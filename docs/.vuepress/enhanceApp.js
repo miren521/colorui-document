bd2279ba769754e235ac7555d0e3fe484a9d205a
miren <2286101414@qq.com>
2025-08-02 12:20:50 +0800

    提交

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,8 +1,10 @@
 // .vuepress/enhanceApp.js
 export default ({ router }) => {
-  router.afterEach(() => {
-    if (window.adsbygoogle) {
-      (adsbygoogle = window.adsbygoogle || []).push({});
-    }
-  });
+  if (process.client) {  // 仅客户端执行
+    router.afterEach(() => {
+      if (window.adsbygoogle) {
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      }
+    });
+  }
 };
